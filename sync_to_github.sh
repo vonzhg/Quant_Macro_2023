f2c40fc877962442b7b7b4042343e7acc2bbab44
vonzhg <z.feng2@gmail.com>
2023-06-09 16:34:49 -0500

    Update Quant macro 2023

---
+++ sync_to_github.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# change to your repository's directory
+cd /Users/zfeng/Library/CloudStorage/OneDrive-UniversityofNebraskaatOmaha/GitHub/Quant_macro
+
+# stage all changes
+git add .
+
+# commit changes
+git commit -m "Update Quant macro 2023"
+
+# pull latest changes from GitHub
+#git pull origin main
+
+# push local changes to GitHub
+git push origin master
