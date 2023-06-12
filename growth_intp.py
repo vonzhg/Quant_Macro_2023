756d4aabc4aee4902d04abe0db73ac080eacf8c5
vonzhg <z.feng2@gmail.com>
2023-06-12 10:36:41 -0500

    Update Quant macro 2023

--- growth_intp.py
+++ growth_intp.py
@@ -6,6 +6,8 @@ from scipy.optimize import minimize
 alpha = 0.4
 beta = 0.96
 tol = 1e-6
+max_iter = 1000  # Maximum number of iterations
+penalty = 1e4  # Penalty term for the objective function
 
 # Discretized state space
 k_min = 0.01
@@ -16,7 +18,8 @@ k_grid = np.linspace(k_min, k_max, n_k)
 # Initial guess for value function
 V_old = np.zeros(n_k)
 
-while True:
+# Iterate over maximum number of iterations
+for iter_num in range(max_iter):
     # Allocate memory to store new value function
     V_new = np.zeros(n_k)
 
@@ -28,7 +31,8 @@ while True:
         k = k_grid[i]
 
         # Objective function for minimization
-        obj = lambda k_plus: -(np.log(k ** alpha - k_plus) + beta * np.interp(k_plus, k_grid, V_old))
+        obj = lambda k_plus: -(np.log(k ** alpha - k_plus) + beta * np.interp(k_plus, k_grid,
+                                                                              V_old)) if k ** alpha - k_plus > 0 else penalty
 
         # Find the optimal k_plus given current k
         result = minimize(obj, x0=0.1, bounds=[(0, k ** alpha)])
@@ -39,10 +43,16 @@ while True:
 
     # Check the convergence criteria
     if np.max(np.abs(V_old - V_new)) < tol:
+        print(f'Convergence achieved after {iter_num} iterations')
         break
 
     # Update value function
     V_old = V_new.copy()
+
+    # If maximum iteration reached
+    if iter_num == max_iter - 1:
+        print("Maximum iterations reached without convergence.")
+        break
 
 # Plot value function
 plt.figure()
